df2010bbff23e861b87c8ffbb208e4315b261f13
Ali Gareeb <Ali Gareeb@DESKTOP-VUBO14J>
2024-04-27 21:11:00 +0300

    Add project files.

---
+++ WebApplicationRest/Controllers/AdminController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationRest.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Table()
+        {
+            return View();
+        }
+        public IActionResult Billing()
+        {
+            return View();
+        }
+        public IActionResult Notification()
+        {
+            return View();
+        }
+        public IActionResult Profile()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ WebApplicationRest/Controllers/RegisterAndLogincs.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationRest.Controllers
+{
+    public class RegisterAndLogincs : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
